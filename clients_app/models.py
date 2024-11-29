df84075677ae9fcc823bdfe4804595d9696e387a
David Alvarez <a.21308051280310@cbtis128.edu.mx>
2024-11-28 22:44:47 -0700

    Add files via upload

---
+++ clients_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Client(models.Model):
+    client_id=models.CharField(primary_key=True,max_length=10)
+    client_name=models.CharField(max_length=50)
+    client_rating=models.IntegerField()
+
+    def __str__(self):
+        return self.client_name
